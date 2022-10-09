feff0daa4bf35a41b316b158f9ca6613a085c00b
TAKE Takashi <nakamizo.takashi@gmail.com>
2022-10-09 16:42:36 +0000

    edit sample

--- infra/main.ts
+++ infra/main.ts
@@ -1,11 +1,15 @@
 import { Construct } from "constructs";
 import { App, TerraformStack } from "cdktf";
+import { s3Bucket as s3 } from "@cdktf/provider-aws";
 
 class MyStack extends TerraformStack {
   constructor(scope: Construct, name: string) {
     super(scope, name);
 
     // define resources here
+    new s3.S3Bucket(this, "myBucket", {
+      bucketPrefix: "example-",
+    });
   }
 }
 
