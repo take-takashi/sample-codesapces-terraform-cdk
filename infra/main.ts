aceae6f602925ea7060be2461c5fca5ea9ea4a5a
TAKE Takashi <nakamizo.takashi@gmail.com>
2022-10-12 16:54:56 +0000

    feat: S3ポリシーとCloudFront作成

--- infra/main.ts
+++ infra/main.ts
@@ -38,23 +38,82 @@ class MyStack extends TerraformStack {
       },
     )
 
+    const oai = new aws.cloudfrontOriginAccessIdentity.CloudfrontOriginAccessIdentity(
+      this,
+      'oai',
+      {},
+    )
+
+    // S3バケットポリシーの定義
+    const policy = new aws.dataAwsIamPolicyDocument.DataAwsIamPolicyDocument(
+      this,
+      'policy_document',
+      {
+        statement: [
+          {
+            actions: ['s3:GetObject'],
+            resources: [`${bucket.arn}/*`],
+            principals: [
+              {
+                type: 'AWS',
+                identifiers: [oai.iamArn],
+              },
+            ],
+          },
+        ],
+      },
+    )
+
     // 静的WEBホスティングのためのバケットボリシー
     new aws.s3BucketPolicy.S3BucketPolicy(this, 'S3BucketPolicy', {
       bucket: bucket.bucket,
-      policy: `
+      policy: policy.json,
+    })
+
+    // Cloudfront
+    new aws.cloudfrontDistribution.CloudfrontDistribution(this, 'distribution', {
+      enabled: true,
+      defaultRootObject: 'index.html',
+      // aliases
+      customErrorResponse: [
         {
-          "Version": "2012-10-17",
-          "Statement": [
-              {
-                  "Sid": "PublicReadGetObject",
-                  "Effect": "Allow",
-                  "Principal": "*",
-                  "Action": "s3:GetObject",
-                  "Resource": "${bucket.arn}/*"
-              }
-          ]
-        }
-      `,
+          errorCode: 403,
+          responseCode: 200,
+          responsePagePath: '/',
+        },
+      ],
+      origin: [
+        {
+          originId: bucket.id,
+          domainName: bucket.bucketRegionalDomainName,
+          s3OriginConfig: {
+            originAccessIdentity: oai.cloudfrontAccessIdentityPath,
+          },
+        },
+      ],
+      defaultCacheBehavior: {
+        allowedMethods: ['GET', 'HEAD'],
+        cachedMethods: ['GET', 'HEAD'],
+        targetOriginId: bucket.id,
+        forwardedValues: {
+          queryString: false,
+          cookies: {
+            forward: 'none',
+          },
+        },
+        viewerProtocolPolicy: 'redirect-to-https',
+        minTtl: 0,
+        defaultTtl: 0,
+        maxTtl: 0,
+      },
+      restrictions: {
+        geoRestriction: {
+          restrictionType: 'none',
+        },
+      },
+      viewerCertificate: {
+        cloudfrontDefaultCertificate: true,
+      },
     })
   }
 }
